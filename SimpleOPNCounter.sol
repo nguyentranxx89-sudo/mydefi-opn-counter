5719fd064e4e2d1ad1b44db555b7e2f563e94ec3
nguyentranxx89-sudo <nguyentranxx89@gmail.com>
2026-05-31 21:51:41 +0700

    Update SimpleOPNCounter.sol
    Simple Counter smart contract for OPN Testnet
    - increment()
    - decrement()
    Deployed for IOPn Builders Season 1 (DeFi & Open Finance)

--- SimpleOPNCounter.sol
+++ SimpleOPNCounter.sol
@@ -1,1 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+contract SimpleOPNCounter {
+    uint256 public count = 0;
+
+    function increment() public {
+        count += 1;
+    }
+
+    function decrement() public {
+        require(count > 0, "Count cannot be negative");
+        count -= 1;
+    }
+}
